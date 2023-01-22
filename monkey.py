667609190508ce36c01f17b2e55bff4e5faace03
quintentwisk <quinten.twisk@hotmail.com>
2023-01-22 21:14:39 +0100

    Add monkey
    Signed-off-by: quintentwisk <quinten.twisk@hotmail.com>

---
+++ monkey.py
@@ -0,0 +1,47 @@
+"""
+Python script that prints out ascii art of a monkey
+"""
+
+print("""
+                                                                              
+                                                                              
+                                                                              
+                                                                              
+                                                                              
+                                                                              
+                                                                              
+                                                                              
+                                                                              
+                                                                              
+                                                                              
+                                                                              
+                                                                              
+                                                                              
+                                                                              
+                                                                              
+                                                                              
+                               .-=-.                                           
+                             .'     '.                                         
+                            :  O   O  :                                        
+                            :   Y7   :                                        
+                            :       :                                          
+                            :       :                                          
+                            '._   _.'                                          
+                              '-'                                             
+                                                                              
+                                                                              
+                                                                              
+                                                                              
+                                                                              
+                                                                              
+                                                                              
+                                                                              
+                                                                              
+                                                                              
+                                                                              
+                                                                              
+                                                                              
+                                                                              
+                                                                              
+                                                                              
+""")
